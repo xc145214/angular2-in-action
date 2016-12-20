0d8a8885040c8699fa55ac5b38332da7b6fe0f04
xc145214 <xc145214@gmail.com>
2016-12-21 00:15:13 +0800

    ng for

--- first_app/angular2-reddit-base/app/app.component.ts
+++ first_app/angular2-reddit-base/app/app.component.ts
@@ -2,12 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `<h1>Hello {{name}}</h1>`
+    template: `
+    <h1>Hello {{name}}</h1>
+    <ul>
+        <li *ngFor="let name of names">hello {{ name }}</li>
+    </ul>
+    `
 })
 export class AppComponent {
     name: string;
+    names: string[];
 
-    constructor(){
+    constructor() {
         this.name = "Angular";
+        this.names = ['Ari', 'Carlos', 'Felipe', 'Nate'];
     }
 }
