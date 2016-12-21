b542eb47ec0263312252d779e46c5dd05ccd42e5
xc145214 <xc145214@gmail.com>
2016-12-21 18:17:30 +0800

    service

--- tour/HEROES/app/app.component.ts
+++ tour/HEROES/app/app.component.ts
@@ -1,20 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from './hero';
-
-
-
-const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
+import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-app',
@@ -90,23 +77,37 @@ const HEROES: Hero[] = [
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
+  `],
+    providers: [HeroService]
 })
 
 
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'Tour of Heroes';
     hero: Hero = {
         id: 1,
         name: 'Windstorm'
     };
-
     //选中
     selectedHero: Hero;
+    heroes: Hero[];
 
-    heroes: Hero[] = HEROES;
+
+    constructor(private heroService: HeroService) {
+
+    }
+
+    getHeroes(): void {
+        this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+    }
 
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
     }
+
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+
+
 }
