af8ec452d69086905e6b17c95bfd74900633d86f
xc145214 <xc145214@gmail.com>
2016-12-21 15:05:42 +0800

    reddit article

--- first_app/angular2-reddit-base/app/app.component.ts
+++ first_app/angular2-reddit-base/app/app.component.ts
@@ -4,9 +4,28 @@ import { Component } from '@angular/core';
     selector: 'my-app',
     template: `
     <h1>Hello {{name}}</h1>
+    <hr/>
     <ul>
         <li *ngFor="let name of names">hello {{ name }}</li>
     </ul>
+    <hr/>
+    <form class ="ui large from segment">
+        <h3 class="ui header">Add a Link</h3>
+
+        <div class="field">
+            <label for="title">Title:</label>
+            <input name="title" #newtitle>
+        </div>
+        <div class="field">
+            <label for="link">Link:</label>
+            <input name="link" #newlink>
+        </div>
+
+        <button (click)="addArticle(newtitle, newlink)"
+        class="ui positive right floated button">
+            Submit link
+        </button>
+    </form>
     `
 })
 export class AppComponent {
@@ -16,5 +35,9 @@ export class AppComponent {
     constructor() {
         this.name = "Angular";
         this.names = ['Ari', 'Carlos', 'Felipe', 'Nate'];
+    }
+
+    addArticle(title:HTMLInputElement,link:HTMLInputElement): void{
+        console.log(`Adding article title: ${title.value} and link: ${link.value}`);
     }
 }
